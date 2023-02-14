31181e345b9d05c27a92921bd942905e41bfa590
urmatgit <urmat2g@gmail.com>
2023-02-14 17:24:25 +0300

    add total query

---
+++ src/Core/Application/Catalog/Totals/TotalByRuralGovDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Application.Catalog.Totals;
+public class TotalByRuralGovDto:IDto
+{
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public string RuralGovName { get; set; }
+    public double Summa { get; set; }
+}
+public class TotalByNative : TotalByRuralGovDto
+{
+     public string FIO { get; set; }
