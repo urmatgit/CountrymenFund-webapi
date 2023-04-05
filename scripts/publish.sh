b10f72819504f87242e5ca2e0c8aaf14b1e3a6b7
urmatgit <urmat2g@gmail.com>
2023-04-05 14:22:48 +0300

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,9 +3,9 @@ echo "start publish"
 dotnet publish /projects/CountrymenFund-webapi/src/Host/Host.csproj -r linux-x64 -o published
 dotnet publish /projects/CountrymenFund-wasm/src/Host/Host.csproj -r linux-x64 -o published
 systemctl stop kestrel-fundWebApi.service
-systemctl stop Wasm.service
+systemctl stop fundWasm.service
 cp -rT /projects/CountrymenFund-webapi/src/Host/published/ /var/www/fundapi/
 cp -rT /projects/CountrymenFund-wasm/src/Host/published/ /var/www/fundwasm/
 systemctl start kestrel-fundWebApi.service
-systemctl start Wasm.service
+systemctl start fundWasm.service
 systemctl restart nginx
