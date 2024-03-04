8268bcba047f9e55edf6f230b1d34b2f0d9c23c5
urmatgit <urmat2g@gmail.com>
2024-03-04 09:15:32 +0300

    Update getRep.sh

--- scripts/getRep.sh
+++ scripts/getRep.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "start get from git"
-cd /project/CountymenFund-wasm/
-git pull git@github.com:urmatgit/CountrymenFund-wasm.git
-cd /project/CountrymenFund-webapi/
-git reset --hard origin
-git pull git@github.com:urmatgit/CountrymenFund-webapi.git
-cd scripts
-chmod +x publish.sh
-./publish.sh
+cd ~/projects/CountymenFund-wasm/
+git pull origin
+cd ~/projects/CountrymenFund-webapi/
+git pull origin
+docker-compose down
+docker-compose up
+
