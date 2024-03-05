0a5b7d01de5db82b741e62477966d5430fbc0b02
urmatgit <urmat2g@gmail.com>
2024-03-05 13:09:01 +0300

    Update dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,6 +7,6 @@ fi
 openssl req -x509 -out dev.crt -keyout dev.key -days 825 \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=*81.94.159.193' -extensions EXT -config <( \
-   printf "[dn]\nCN=*81.94.159.193\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:81.94.159.193\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   printf "[dn]\nCN=81.94.159.193\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:81.94.159.193\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 openssl pkcs12 -export -out aspnetapp1.pfx -inkey dev.key -in dev.crt -password pass:$PASSWORD
